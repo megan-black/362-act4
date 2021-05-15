5bb34891eb598a18a98da3de4d9ce758ad28ed4d
megan black <blackme@oregonstate.edu>
2021-05-15 11:53:02 -0700

    Added palindrome code

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+
+def is_pal():
+    string = input("Enter a string: ")
+    if(string == string[::-1]):
+        print("The string is a palindrome")
+    else:
+        print("The string is not a palindrome")
+
+is_pal()

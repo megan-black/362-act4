2c22bf10d41a1c65d32111bc2b1763045e9facdb
megan black <blackme@oregonstate.edu>
2021-05-15 12:18:58 -0700

    Removed palindrome.py and replaced with test_palindrome.py

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-
-def is_pal():
-    string = input("Enter a string: ")
-    if(string == string[::-1]):
-        print("The string is a palindrome")
-    else:
-        print("The string is not a palindrome")
-
-is_pal()
